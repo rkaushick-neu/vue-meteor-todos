263de2922c63f3c68d4a23092377c2d55a54f97e
Rishabh Kaushick <kaushick.r@northeastern.edu>
2026-01-29 00:35:15 -0500

    subscribed to tasks using subscribe & autorun on Vue

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -1,14 +1,10 @@
-<!-- adding sample data here instead of getting it from the database -->
 <script setup>
 import Task from './components/Task.vue';
+import { subscribe, autorun } from 'vue-meteor-tracker';
+import { TasksCollection } from '../api/taskCollection';
 
-const getTasks = () => {
-  return [
-    { _id: 1, text: 'Install Node.js'},
-    { _id: 2, text: 'Install Meteor 3'},
-    { _id: 3, text: 'Create and run your project'},
-  ];
-}
+subscribe('tasks');
+const tasks = autorun(() => TasksCollection.find({}).fetch()).result;
 </script>
 
 <template>
@@ -17,10 +13,7 @@ const getTasks = () => {
       <h1 class="text-4xl font-bold text-gray-800 my-4"">Todo List</h1>
     </header>
     <ul class="list-disc list-inside p-4">
-      <Task v-for="task in getTasks()" :key="task._id" :task="task" />
+      <Task v-for="task of tasks" :key="task._id" :task="task" />
     </ul>
-  </div>
-  <div class="p-8">
-    <h1>Hello Meteor</h1>
   </div>
 </template>
